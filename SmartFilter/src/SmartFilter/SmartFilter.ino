44c4cc1dd8b6b09dbf89cff8ed668445d6b58d7d
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2019-10-16 18:56:24 -0300

    Ajustando codigo fonte com pushFloat

---
+++ SmartFilter/src/SmartFilter/SmartFilter.ino
@@ -0,0 +1,31 @@
+#include <FirebaseArduino.h>
+ 
+#define FIREBASE_HOST "SmartFilter_PIIV"
+#define FIREBASE_AUTH "VKIZQ2q335nXFk8BdWrlreM1vcx2uhzRyTQWMhq1"
+#define WIFI_SSID "Lem"
+#define WIFI_PASSWORD "naolembro63a"
+ 
+void setup() {
+  Serial.begin(9600);
+ 
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("conectando");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("conectado: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+void loop() {
+  
+  int sensorValue = analogRead(A0);   // Ler o pino Analógico A0 onde está o LDR
+  float voltagem = sensorValue * (5 / 1024.0);   // Converter a leitura analógica (que vai de 0 - 1023) para uma voltagem (0 - 3.3V), quanto de acordo com a intensidade de luz no LDR a voltagem diminui.
+  Serial.println(voltagem);   // Mostrar valor da voltagem no monitor serial
+  Firebase.pushFloat("Tensao", voltagem); // Envia o dado da variavel voltagem para a variavel no Firebase
+  delay(5000); 
+  
+}
